aebed34252b765515881c44de2752c5fc7e323b8
Sadiq <sadiq.said@student.moringaschool.com>
2023-07-21 16:58:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }  
+  console.log(saturdayFun('roller-skate'));
+
+  function mondayWork(task = 'go to the office') {
+    return `This Monday, I will ${task}.`;
+  }
+  console.log(mondayWork('go to the office'));
+ 
+  function wrapAdjective(flair = '*') {
+    return function (adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}`;
+    };
+  }
+  const encouragingPromptFunction = wrapAdjective('!!!');
+  console.log(encouragingPromptFunction('a dedicated programmer'));
